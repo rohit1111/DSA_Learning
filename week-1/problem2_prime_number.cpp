e36902d6d858fbe1f7fd4090acac007abc4aefb4
rohit1111 <rohit.jagtap10@gmail.com>
2025-11-05 11:15:01 +0530

    Create problem2_prime_number.cpp

---
+++ week-1/problem2_prime_number.cpp
@@ -0,0 +1,35 @@
+// Problem Link: https://www.geeksforgeeks.org/problems/prime-number2314/1
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if(n == 1)
+            return false;
+        for(int i=2;i<n;i++){
+            if(n%i == 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+
+/*
+Prime Number
+Difficulty: EasyAccuracy: 22.2%Submissions: 423K+Points: 2
+Given a number n, determine whether it is a prime number or not.
+Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+Examples :
+
+Input: n = 7
+Output: true
+Explanation: 7 has exactly two divisors: 1 and 7, making it a prime number.
+Input: n = 25
+Output: false
+Explanation: 25 has more than two divisors: 1, 5, and 25, so it is not a prime number.
+Input: n = 1
+Output: false
+Explanation: 1 has only one divisor (1 itself), which is not sufficient for it to be considered prime.
+Constraints:
+1 ≤ n ≤ 109
+*/

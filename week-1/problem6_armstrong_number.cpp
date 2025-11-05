95d80a90659cb32346b3c079de02673116d5941e
rohit1111 <rohit.jagtap10@gmail.com>
2025-11-05 20:13:44 +0530

    Create problem6_armstrong_number.cpp

---
+++ week-1/problem6_armstrong_number.cpp
@@ -0,0 +1,40 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1
+
+class Solution {
+  public:
+    bool armstrongNumber(int n) {
+        int result = 0;
+        int copyN = n;
+        while(n>0){
+            int digit = n%10;
+            result += digit * digit * digit;
+            n = n / 10;
+        }
+        if(result == copyN)
+            return true;
+        else
+            return false;
+    }
+};
+
+/*
+Armstrong Numbers
+Difficulty: EasyAccuracy: 49.88%Submissions: 210K+Points: 2
+You are given a 3-digit number n, Find whether it is an Armstrong number or not.
+
+An Armstrong number of three digits is a number such that the sum of the cubes of its digits is equal to the number itself. 371 is an Armstrong number since 33 + 73 + 13 = 371. 
+
+Examples:
+
+Input: n = 153
+Output: true
+Explanation: 153 is an Armstrong number since 13 + 53 + 33 = 153. 
+Input: n = 372
+Output: false
+Explanation: 372 is not an Armstrong number since 33 + 73 + 23 = 378. 
+Input: n = 100
+Output: false
+Explanation: 100 is not an Armstrong number since 13 + 03 + 03 = 1. 
+Constraints:
+100 ≤ n <1000 
+*/

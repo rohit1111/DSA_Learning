6070dcb6b3fa5e31990ddd2dfba7b779b569e886
rohit1111 <rohit.jagtap10@gmail.com>
2025-11-05 11:10:12 +0530

    Create problem1_addition_of_two_num.cpp

---
+++ week-1/problem1_addition_of_two_num.cpp
@@ -0,0 +1,29 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/addition-of-two-numbers0812/1
+
+class Solution {
+  public:
+    int addition(int a, int b) {
+        return a + b;  
+    }
+};
+
+
+/*
+Addition of Two Numbers
+Difficulty: BasicAccuracy: 79.5%Submissions: 79K+Points: 1
+Given two numbers a and b. Your task is to return the sum of a and b.
+
+Examples:
+
+Input: a = 1, b = 2
+Output: 3
+Explanation: Addition of 1 and 2 is 3.
+Input: a = 10, b = 20
+Output: 30
+Explanation: Addition os 10 and 20 is 30.
+Constraints:
+1 <= a, b <= 1018
+
+
+
+*/

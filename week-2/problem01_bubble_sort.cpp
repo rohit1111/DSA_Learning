83f2756b73758fc454647d628ae81169ecd505ba
rohit1111 <rohit.jagtap10@gmail.com>
2025-11-10 14:40:55 +0530

    Create problem01_bubble_sort.cpp

---
+++ week-2/problem01_bubble_sort.cpp
@@ -0,0 +1,43 @@
+//Problem Link: https://www.geeksforgeeks.org/problems/bubble-sort/1
+
+class Solution {
+  public:
+    void bubbleSort(vector<int>& arr) {
+        int n = arr.size();
+        for(int i=0;i<n-1;i++){
+            int isSwapped = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    swap(arr[j], arr[j+1]);
+                    isSwapped = true;
+                }
+            }
+            if(isSwapped == false){
+                break;
+            }
+        }
+        
+    }
+};
+
+
+/*
+Bubble Sort
+Difficulty: EasyAccuracy: 59.33%Submissions: 327K+Points: 2Average Time: 15m
+Given an array, arr[]. Sort the array using bubble sort algorithm.
+
+Examples :
+
+Input: arr[] = [4, 1, 3, 9, 7]
+Output: [1, 3, 4, 7, 9]
+Explanation: After Sorting the array in ascending order of their values is [1, 3, 4, 7, 9].
+Input: arr[] = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Explanation: Sort the array in ascending order of their values.
+Input: arr[] = [1, 2, 3, 4, 5]
+Output: [1, 2, 3, 4, 5]
+Explanation: An array that is already sorted should remain unchanged after applying bubble sort.
+Constraints:
+1 ≤ arr.size() ≤ 103
+1 ≤ arr[i] ≤ 103
+*/

70585a6f6b3f154c1d57996a4e8bf6bcb4ee72a2
rohit1111 <rohit.jagtap10@gmail.com>
2025-11-12 19:00:19 +0530

    Create problem2_search_insert_position.cpp

---
+++ week-2/problem2_search_insert_position.cpp
@@ -0,0 +1,66 @@
+//Problem Link: https://leetcode.com/problems/search-insert-position/
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start = 0;
+        int end = nums.size()-1;
+        int positionInsert = 0;
+        int mid = 0;
+        while(start<=end){
+            mid = start + (end -start)/2;
+            cout<<start<<" "<<end<<" "<<mid<<" "<<positionInsert<<endl;
+            if(target == nums[mid]){
+                //return mid;
+                positionInsert = mid;
+                return mid;
+            }
+            else if(target < nums[mid]){
+                end = mid -1;
+            }
+            else if(target > nums[mid]){
+                start = mid +1;
+            }
+            //else{
+            //    cout<<"For"<<endl;
+            //    return mid;
+            //}
+        }
+        
+        return start;
+    }
+};
+
+/*
+35. Search Insert Position
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+*/

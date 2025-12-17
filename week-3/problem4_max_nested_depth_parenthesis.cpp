71c35c3f8fc8515edaf029964c938a57a94405b4
rohit1111 <rohit.jagtap10@gmail.com>
2025-12-17 23:32:58 +0530

    Create problem4_max_nested_depth_parenthesis.cpp

---
+++ week-3/problem4_max_nested_depth_parenthesis.cpp
@@ -0,0 +1,66 @@
+// Problem Link: https://leetcode.com/problems/maximum-nesting-depth-of-the-parentheses/description/
+class Solution {
+public:
+    int maxDepth(string s) {
+       int n = s.size();
+       int countP = 0;
+       int maxP=0;
+       for(int i=0;i<n;i++){
+            if(s[i] == '('){
+                countP++;
+            }else if(s[i] == ')'){
+                countP--;
+            }
+            maxP = max(countP,maxP);
+       }
+       return maxP;
+    }
+};
+
+
+/*
+1614. Maximum Nesting Depth of the Parentheses
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+Given a valid parentheses string s, return the nesting depth of s. The nesting depth is the maximum number of nested parentheses.
+
+ 
+
+Example 1:
+
+Input: s = "(1+(2*3)+((8)/4))+1"
+
+Output: 3
+
+Explanation:
+
+Digit 8 is inside of 3 nested parentheses in the string.
+
+Example 2:
+
+Input: s = "(1)+((2))+(((3)))"
+
+Output: 3
+
+Explanation:
+
+Digit 3 is inside of 3 nested parentheses in the string.
+
+Example 3:
+
+Input: s = "()(())((()()))"
+
+Output: 3
+
+ 
+
+Constraints:
+
+1 <= s.length <= 100
+s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+It is guaranteed that parentheses expression s is a VPS.
+*/
